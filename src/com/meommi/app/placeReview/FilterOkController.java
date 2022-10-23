492fa9cf515bb02377743cad262c29c723bdd1b3
ghdwnstjd <dltmdgheorkf1@gmail.com>
2022-10-23 21:23:41 +0900

    지도 필터 정보 및 수정, 삭제 등 기능 추가, 이미지 첨부 오류 수정

---
+++ src/com/meommi/app/placeReview/FilterOkController.java
@@ -0,0 +1,19 @@
+package com.meommi.app.placeReview;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.meommi.app.Execute;
+import com.meommi.app.Result;
+
+public class FilterOkController implements Execute {
+
+	@Override
+	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		return null;
+	}
+}
